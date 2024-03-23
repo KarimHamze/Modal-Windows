232b98d981e4e4a41ba53932e3baa0909abeeedc
KarimHamze <kariham9@gmail.com>
2024-03-23 19:03:51 -0300

    Practica terminada

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+console.log(btnsOpenModal);
+
+// Loop para que el modal aplique a los 3 botones
+for (let i = 0; i < btnsOpenModal.length; i++)
+  // Abrir ventana
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// Cerrar ventana con Click en la "X" y en el Overlay
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// Cerrar ventana con escape
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
